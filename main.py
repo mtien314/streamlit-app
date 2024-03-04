00eb7a1f70cd3397e67e48675b59ae737edf5cba
mtien314 <90877954+mtien314@users.noreply.github.com>
2024-03-04 10:53:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -169,11 +169,10 @@ if selected == "Login":
 
 
         try:
-            if result ==True:
-                email_of_registered_user, username_of_registered_user, name_of_registered_user = authenticator.register_user(
+            email_of_registered_user, username_of_registered_user, name_of_registered_user = authenticator.register_user(
                         preauthorization=False)
-                if email_of_registered_user:
-                    st.success('User registered successfully')
+            if email_of_registered_user:
+                st.success('User registered successfully')
         except Exception as e:
             st.error(e)
         
