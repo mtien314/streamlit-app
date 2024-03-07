92ecc339c895aa3a6f5ca143d453bb11ed664b0d
mtien314 <90877954+mtien314@users.noreply.github.com>
2024-03-07 17:35:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -182,15 +182,9 @@ if selected == "Login":
             try:
                 if authenticator.reset_password(st.session_state["username"]):
                     st.success('Password modified successfully')
-                    #st.session_state["name"] = name_of_registered_user
-
-                    # Save the new username to the config file
-                    #with open('config.yaml', 'w') as file:
-                        #yaml.dump(config, file, default_flow_style=False)
             except Exception as e:
                 st.error(e)
         
-
         # Creating an update user details widget
         if st.session_state["authentication_status"]:
             try:
