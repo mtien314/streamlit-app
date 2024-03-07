0e63a26c6ddbfc6c0bb21fd5b1a7f3f9ccaee413
mtien314 <90877954+mtien314@users.noreply.github.com>
2024-03-07 17:00:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -178,7 +178,7 @@ if selected == "Login":
         #st.session_state["register_clicked"] = False
         
         # Creating a password reset widget
-        if st.session_state["authentication_status"] is True:
+        if st.session_state["authentication_status"]:
             try:
                 if authenticator.reset_password(st.session_state["username"]):
                     st.success('Password modified successfully')
@@ -187,16 +187,10 @@ if selected == "Login":
         
 
         # Creating an update user details widget
-        if st.session_state["authentication_status"] is True:
+        if st.session_state["authentication_status"]:
             try:
                 if authenticator.update_user_details(st.session_state["username"]):
                     st.success('Entries updated successfully')
-                            
-                    config['credentials']['username'] = username_of_registered_user
-                    st.session_state["name"] = name_of_registered_user
-                # Save the new username to the config file
-                    with open('config.yaml', 'w') as file:
-                        yaml.dump(config, file, default_flow_style=False)
             except Exception as e:
                 st.error(e)
         
