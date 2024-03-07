a1ae29df1770ad5a20afbbf3044bdb0967854001
mtien314 <90877954+mtien314@users.noreply.github.com>
2024-03-07 17:19:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -163,20 +163,21 @@ if selected == "Login":
             st.warning('Please Enter Username/password')
             result = st.button('Have not account ?')
         
-        if result:
-            st.session_state["register_clicked"] = True
+            if result:
+                st.session_state["register_clicked"] = True
             
-        if st.session_state.get("register_clicked",False):
-            try:
-                email_of_registered_user, username_of_registered_user,name_of_registered_user = authenticator.register_user(
-                preauthorization=False)    
-                if email_of_registered_user:
-                    st.success('User registered successfully')
+            if st.session_state.get("register_clicked",False):
+        
+                try:
+                    email_of_registered_user, username_of_registered_user,name_of_registered_user = authenticator.register_user(
+                    preauthorization=False)    
+                    if email_of_registered_user:
+                        st.success('User registered successfully')
                     
-            except Exception as e:
+                except Exception as e:
                     st.error(e)
                 
-        st.session_state["register_clicked"] = False
+        
         
         # Creating a password reset widget
         if st.session_state["authentication_status"]:
